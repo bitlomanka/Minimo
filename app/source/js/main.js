a6eebebe2a3ace82219928dda1db31d265f2e22a
Виктория Кравченко <vica@MacBook-Pro-Sergey.local>
2016-06-17 00:39:08 +0300

    the latest version

--- app/source/js/main.js
+++ app/source/js/main.js
@@ -2,7 +2,22 @@
 $(document).ready(function(){
 
   
+    // mobile-navigation
     
+    $('.mobile-navigation').on("click", function(e){
+        e.stopPropagation();
+        var menu = $('.header').find('.navigation-list');
+        
+        if(menu.is(":hidden")){
+            menu.slideDown(400);
+        }else{
+            menu.slideUp(400);
+        }
+    });
+    
+    //ie placeholder
+    
+    $('input, textarea').placeholder();
     
 });
 
